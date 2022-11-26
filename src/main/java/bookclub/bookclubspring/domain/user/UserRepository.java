90cad3f6e6a44bb852e5271347192accb012d34d
littleemm <lilysolar@naver.com>
2022-11-26 22:04:30 +0900

    [ADD] 회원 정보를 게시글에 넘겨주는 코드 추가

--- src/main/java/bookclub/bookclubspring/domain/user/UserRepository.java
+++ src/main/java/bookclub/bookclubspring/domain/user/UserRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    /* OAuth */
     Optional<User> findByEmail(String email);
 
+    /* user GET */
     User findByName (String name);
 
     boolean existsByName(String name);
--- src/main/java/bookclub/bookclubspring/web/ReviewController.java
+++ src/main/java/bookclub/bookclubspring/web/ReviewController.java
@@ -2,6 +2,7 @@ package bookclub.bookclubspring.web;
 
 import bookclub.bookclubspring.config.auth.LoginUser;
 import bookclub.bookclubspring.config.auth.dto.SessionUser;
+import bookclub.bookclubspring.domain.posts.Posts;
 import bookclub.bookclubspring.domain.user.User;
 import bookclub.bookclubspring.service.posts.PostService;
 import bookclub.bookclubspring.web.dto.PostsResponseDto;
@@ -13,35 +14,33 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-public class PostsApiController {
-
+public class ReviewController {
     private final PostService postService;
 
     /* CREATE */
-    @PostMapping("/api/v1/posts")
-    public ResponseEntity save(User user, @RequestBody PostsSaveRequestDto requestDto, @LoginUser SessionUser sessionUser) {
+    @PostMapping("/api/v1/reviews")
+    public ResponseEntity save(Posts posts, User user, @RequestBody PostsSaveRequestDto requestDto, @LoginUser SessionUser sessionUser) {
 
         String writer = sessionUser.getName();
         return ResponseEntity.ok(postService.save(writer, requestDto));
     }
 
-    @PutMapping("/api/v1/posts/{id}")
+    @PutMapping("/api/v1/reviews/{id}")
     public Long update(@PathVariable Long id, @RequestBody
     PostsUpdateRequestDto requestDto) {
 
         return postService.update(id, requestDto);
     }
 
-    @GetMapping("/api/v1/posts/{id}")
+    @GetMapping("/api/v1/reviews/{id}")
     public PostsResponseDto findById (@PathVariable Long id) {
 
         return postService.findById(id);
     }
 
-    @DeleteMapping("/api/v1/posts/{id}")
+    @DeleteMapping("/api/v1/reviews/{id}")
     public Long delete(@PathVariable Long id) {
         postService.delete(id);
         return id;
     }
-
 }
